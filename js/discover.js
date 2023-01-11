ac0f4106bd5ab8fa246fce47e252e1bcc2be934e
moonl <moonlitbetelion@hotmail.com>
2023-01-11 13:12:46 +0800

    功能： 1.增加发现最新小说

--- js/discover.js
+++ js/discover.js
@@ -1,1 +1,93 @@
-最新小说::https://linpxapi.linpicio.com/pixiv/novels/recent?page={{page}}
+@js:
+
+function urlSeries(seriesId) {
+    return `https://linpxapi.linpicio.com/pixiv/series/${seriesId}`
+}
+
+function urlNovelsDetailed(nidList) {
+    return `https://linpxapi.linpicio.com/pixiv/novels?${nidList.map(v => "ids[]=" + v).join("&")}`
+}
+
+function getAjaxJson(url) {
+    return cacheGetAndSet(url, () => {
+        return JSON.parse(java.ajax(url))
+    })
+}
+
+function cacheGetAndSet(key, supplyFunc) {
+    let v = cache.get(key)
+    if (v === undefined || v === null) {
+        v = JSON.stringify(supplyFunc())
+        // 缓存10分钟
+        cache.put(key, v, 600)
+    }
+    return JSON.parse(v)
+}
+
+// 存储seriesID
+var seriesSet = new Set();
+
+// 将多个长篇小说解析为一本书
+function combineNovels(novels) {
+    let newNovels = []
+    // key为seriesId value为novel
+    novels.forEach(novel => {
+        // 视为真正的一本书
+        if (novel.seriesId !== undefined && novel.seriesTitle !== undefined) {
+            if (!seriesSet.has(novel.seriesId)) {
+                seriesSet.add(novel.seriesId)
+                newNovels.push(novel)
+            }
+        } else {
+            // 短篇小说
+            newNovels.push(novel)
+        }
+    })
+    return newNovels
+}
+
+// 将小说的封面规则与详情地址替换
+function formatNovels(novels) {
+    novels.forEach(novel => {
+        novel.detailedUrl = `https://linpxapi.linpicio.com/pixiv/novel/${novel.id}`
+        if (novel.seriesId !== undefined && novel.seriesId !== null) {
+            novel.title = novel.seriesTitle
+            //todo 长篇的字数需要统计 先不做
+            novel.length = null
+
+            let series = getAjaxJson(urlSeries(novel.seriesId))
+            // novel.coverUrl = `https://linpxapi.linpicio.com/proxy/pximg?url=${series.imageUrl}`
+            // 后端目前没有系列的coverUrl字段
+            // todo 先这样使用
+            novel.coverUrl = `https://linpxapi.linpicio.com/proxy/pximg?url=${series.novels[0].coverUrl}`
+            if (series.caption === "") {
+                let firstNovels = getAjaxJson(urlNovelsDetailed([series.novels[0].id]))
+                novel.desc = firstNovels[0].desc
+            } else {
+                novel.desc = series.caption
+            }
+
+            //如果没有标签 取第一章的tag
+            if (series.tags.length === 0) {
+                // 系列至少会有一章
+                novel.tags = series.novels[0].tags
+            } else {
+                novel.tags = series.tags
+            }
+            novel.tags.unshift("长篇")
+
+
+        } else {
+            novel.tags.unshift("单本")
+            novel.coverUrl = `https://linpxapi.linpicio.com/proxy/pximg?url=${novel.coverUrl}`
+        }
+
+        novel.tags = novel.tags.join(",")
+    })
+    return novels
+}
+
+
+(function (res) {
+    return formatNovels(combineNovels(JSON.parse(res)))
+}(result))
