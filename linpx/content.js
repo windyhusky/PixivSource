3db593bd670a97a89b51e9e91dccd1ecbc8cdb0e
moonl <moonlitbetelion@hotmail.com>
2023-03-02 19:00:26 +0800

    样式变更

--- linpx/content.js
+++ linpx/content.js
@@ -3,7 +3,7 @@
     res = JSON.parse(res)
     let content = res.content
     if (res.series !== null && res.desc !== undefined && res.desc !== "") {
-        content = res.desc + "\n" + "　\n".repeat(2) + content
+        content = res.desc + "\n" + "——————————\n".repeat(2) + content
     }
     //将存在的pixiv图片链接替换为可访问的直连
     if (res.images !== undefined && res.images !== null) {
