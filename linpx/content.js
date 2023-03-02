09478efaf4a396eafc9fcab99cab03372836b812
moonl <moonlitbetelion@hotmail.com>
2023-03-02 16:58:24 +0800

    修复linpx书源因合并系列对其章节简介缺失问题

--- linpx/content.js
+++ linpx/content.js
@@ -2,7 +2,10 @@
 (function (res) {
     res = JSON.parse(res)
     let content = res.content
-    //链接替换 如果有的话
+    if (res.series !== null && res.desc !== undefined && res.desc !== "") {
+        content = res.desc + "\n" + "　\n".repeat(2) + content
+    }
+    //将存在的pixiv图片链接替换为可访问的直连
     if (res.images !== undefined && res.images !== null) {
         Object.keys(res.images).forEach((key) => {
             content = content.replace(`[uploadedimage:${key}]`, `<img src="https://linpxapi.linpicio.com/proxy/pximg?url=${res.images[key].preview}">`)
