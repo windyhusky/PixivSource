2540e508f6390bcf4438cef10f7b100ab0d68cad
windyhusky <moonlitbetelion@hotmail.com>
2023-03-18 16:09:05 +0800

    pixiv:正文中的图片样式将铺满 linpx:正文中的图片样式将铺满 可尝试导入缓存小说

--- linpx/catalog.js
+++ linpx/catalog.js
@@ -1,9 +1,16 @@
 @js:
 (function (res) {
     res = JSON.parse(res)
+    let cache = function () {
+        if (baseUrl.includes("/cache")) {
+            return "/cache"
+        }
+        return ""
+    }()
+
     if (res.novels !== undefined) {
         res.novels.forEach(v => {
-            v['url'] = `https://linpxapi.linpicio.com/pixiv/novel/${v.id}`
+            v['url'] = `https://linpxapi.linpicio.com/pixiv/novel/${v.id}${cache}`
         })
         return res.novels
     }
--- linpx/detail.js
+++ linpx/detail.js
@@ -7,7 +7,13 @@
             return []
         }
         let id = matchResult[0].replace("novel/", "")
-        res = JSON.parse(java.ajax(`https://linpxapi.linpicio.com/pixiv/novel/${id}`))
+        if (baseUrl.includes("/cache")) {
+            res = JSON.parse(java.ajax(`https://linpxapi.linpicio.com/pixiv/novel/${id}/cache`))
+            // 不获取缓存系列
+            res.series = null
+        } else {
+            res = JSON.parse(java.ajax(`https://linpxapi.linpicio.com/pixiv/novel/${id}`))
+        }
     } else {
         res = JSON.parse(res)
         if (res.total === 0) {
@@ -25,7 +31,11 @@
 
     if (res.series === undefined || res.series === null) {
         prop['name'] = res.title
-        prop['catalog'] = `https://linpxapi.linpicio.com/pixiv/novel/${res.id}`
+        if (baseUrl.includes("/cache")) {
+            prop['catalog'] = `https://linpxapi.linpicio.com/pixiv/novel/${res.id}/cache`
+        } else {
+            prop['catalog'] = `https://linpxapi.linpicio.com/pixiv/novel/${res.id}`
+        }
         res.tags.unshift('单本')
     } else {
         prop['name'] = res.series.title
