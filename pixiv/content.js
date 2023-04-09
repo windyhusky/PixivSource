337295cbd4e295a7c80028ab44be01687a344820
windyhusky <moonlitbetelion@hotmail.com>
2023-04-09 11:12:01 +0800

    pixiv书源：正文图片不再走linpx代理

--- pixiv/content.js
+++ pixiv/content.js
@@ -8,8 +8,8 @@
     let hasEmbeddedImages = res.textEmbeddedImages !== undefined && res.textEmbeddedImages !== null
     if (hasEmbeddedImages) {
         Object.keys(res.textEmbeddedImages).forEach((key) => {
-            //todo 使用了linpx的图片代理 目前只能想到这样解决
-            content = content.replace(`[uploadedimage:${key}]`, `<img src="https://linpxapi.linpicio.com/proxy/pximg?url=${res.textEmbeddedImages[key].urls.original}">`)
+            // content = content.replace(`[uploadedimage:${key}]`, `<img src="https://linpxapi.linpicio.com/proxy/pximg?url=${res.textEmbeddedImages[key].urls.original}">`)
+            content = content.replace(`[uploadedimage:${key}]`, `<img src="${res.textEmbeddedImages[key].urls.original}">`)
         })
     }
     return content
