ab01ec9d63219178ddea2dcab8cfc2f9a8795ccf
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2024-06-02 18:51:51 +0800

    替换 Pixiv 标记符号

--- linpx/content.js
+++ linpx/content.js
@@ -27,5 +27,54 @@
         }
     }
 
+    // 替换 Pixiv 标记符号 [newpage] [chapter:]
+    // 阅读不支持分页符，因此在 `[chapter:]`添加空行
+    content = content.replace(`[newpage]`, `<div style="page-break-after:always"></div>`)
+    matched = content.match(RegExp(/\[chapter:(.*)]/gm))
+    if (matched) {
+        for (let i in matched) {
+            let matched2 = matched[i].match(/\[chapter:(.*)]/m)
+            let chapter = matched2[1].trim()
+            if (("第".includes(chapter))||("章".includes(chapter))){
+                // chapter = `${chapter}`
+            }
+            else {
+                chapter = `第${Number(i)+1}节 ${chapter}`
+            }
+            content = content.replace(`${matched[i]}`, `${"<p>​<p/>".repeat(3)}${chapter}<p>​<p/>`)
+        }
+    }
+
+    // 替换 Pixiv 跳转页面标记符号 `[[jump:]]`
+    matched = content.match(/\[jump:(\d+)]/gm)
+    if (matched) {
+        for (let i in matched) {
+            let page = matched[i].match(/\d+/)
+            content = content.replace(`${matched[i]}`, `\n\n跳转至第${page}节`)
+        }
+    }
+
+    // 替换 Pixiv 链接标记符号 `[[jumpuri: > ]]`
+    matched = content.match(/\[\[jumpuri:(.*)>(.*)]]/gm)
+    if (matched) {
+        for (let i in matched) {
+            let matched2 = matched[i].match(/\[\[jumpuri:(.*)>(.*)]]/m)
+            let matchedText = matched2[0]
+            let urlName = matched2[1].trim()
+            let urlLink = matched2[2].trim()
+            // 阅读不支持超链接
+            //content = content.replace(`${matchedText}`, `<a href=${urlLink}>${urlName}</a>`)
+            if (urlLink === urlName) {
+                content = content.replace(`${matchedText}`, `${urlName}: ${urlLink}`)
+            }
+            else{
+                content = content.replace(`${matchedText}`, `${urlName}`)
+            }
+        }
+    }
+
+
+
+
     return content
 })(result)
--- pixiv/content.js
+++ pixiv/content.js
@@ -14,10 +14,11 @@ function objParse(obj) {
 (() => {
     let res = JSON.parse(result).body
     let content = res.content
-    // 正文内部加入小说描述
+    // 在正文内部添加小说描述
     if (res.seriesNavData !== undefined && res.seriesNavData !== null && res.description !== "") {
         content = res.description + "\n" + "——————————\n".repeat(2) + content
     }
+
     // 获取 [uploadedimage:] 的图片链接
     let hasEmbeddedImages = res.textEmbeddedImages !== undefined && res.textEmbeddedImages !== null
     if (hasEmbeddedImages) {
@@ -41,8 +42,52 @@ function objParse(obj) {
         }
     }
 
-    // 替换 Pixiv 其他标记符号 `[[jumpuri: > ]]`
+
+    // 替换 Pixiv 标记符号 [newpage] [chapter:]
+    // 阅读不支持分页符，因此在 `[chapter:]`添加空行
     content = content.replace(`[newpage]`, `<div style="page-break-after:always"></div>`)
-    // content = content.replace(`[[jumpuri: > ]]`, `<a href="https://www.w3school.com.cn">访问 w3school.com.cn!</a>`)
+    matched = content.match(RegExp(/\[chapter:(.*)]/gm))
+    if (matched) {
+        for (let i in matched) {
+            let matched2 = matched[i].match(/\[chapter:(.*)]/m)
+            let chapter = matched2[1].trim()
+            if (("第".includes(chapter))||("章".includes(chapter))){
+                // chapter = `${chapter}`
+            }
+            else {
+                chapter = `第${Number(i)+1}节 ${chapter}`
+            }
+            content = content.replace(`${matched[i]}`, `${"<p>​<p/>".repeat(3)}${chapter}<p>​<p/>`)
+        }
+    }
+
+    // 替换 Pixiv 跳转页面标记符号 `[[jump:]]`
+    matched = content.match(/\[jump:(\d+)]/gm)
+    if (matched) {
+        for (let i in matched) {
+            let page = matched[i].match(/\d+/)
+            content = content.replace(`${matched[i]}`, `\n\n跳转至第${page}节`)
+        }
+    }
+
+    // 替换 Pixiv 链接标记符号 `[[jumpuri: > ]]`
+    matched = content.match(/\[\[jumpuri:(.*)>(.*)]]/gm)
+    if (matched) {
+        for (let i in matched) {
+            let matched2 = matched[i].match(/\[\[jumpuri:(.*)>(.*)]]/m)
+            let matchedText = matched2[0]
+            let urlName = matched2[1].trim()
+            let urlLink = matched2[2].trim()
+            // 阅读不支持超链接
+            //content = content.replace(`${matchedText}`, `<a href=${urlLink}>${urlName}</a>`)
+            if (urlLink === urlName) {
+                content = content.replace(`${matchedText}`, `${urlName}: ${urlLink}`)
+            }
+            else{
+                content = content.replace(`${matchedText}`, `${urlName}`)
+            }
+        }
+    }
+
     return content
 })()
