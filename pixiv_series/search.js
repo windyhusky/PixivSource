62602a14c6e020ebc3f289ace89ddcc947dccfe5
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2024-11-20 17:34:21 +0800

    Pixiv 系列书源: 优化 handNovels

--- pixiv_series/search.js
+++ pixiv_series/search.js
@@ -52,23 +52,44 @@ function handNovels(novels) {
         if (novel.tags === undefined || novel.tags === null) {
             novel.tags = []
         }
-        // 最大化使用搜索获得的数据
-        novel.textCount = novel.textLength
-        novel.url = novel.cover.urls["480mw"]
-        novel.description = novel.caption
         // novel.tags = novel.tags
 
-        if (novel.isOneshot === true) {  //单篇小说
+        if (novel.isOneshot === true || novel.seriesId === undefined || novel.seriesId === null) {  //单篇小说
+            if (novel.novelId !== undefined) {  // oneshot 单篇
+                novel.id = novel.novelId  // 获取真正的 novelid
+                // novel.title = novel.title
+                novel.url = novel.cover.urls["480mw"]
+                novel.description = novel.caption
+                novel.textCount = novel.textLength
+            }
+            // novel.id = novel.id    // 搜索作者获取的单篇
+            // novel.title = novel.title
+            // novel.url = novel.url
+            // novel.description = novel.description
+            // novel.textCount = novel.textCount
             novel.seriesId = undefined
-            novel.id = novel.novelId
-            novel.name = novel.latestChapter = novel.title
+            novel.latestChapter = novel.title
             novel.tags.unshift("单本")
 
         } else {  // 系列小说
-            novel.seriesId = novel.id  // 真正的系列小说id
-            // novel.id = novel.latestEpisodeId  // 最近一篇
-            // novel.lastChapter = this.getAjaxJson(this.urlNovelDetailed(novel.id)).body.title
-            novel.name = novel.title
+            if (novel.seriesId !== undefined && novel.seriesId !== null) { // 搜索作者获取的系列篇目
+                // novel.id = novel.id
+                novel.id = novel.seriesId
+                // novel.seriesId = novel.seriesId
+                novel.title = novel.seriesTitle
+                // novel.url = novel.url
+                // novel.description = novel.description
+                // novel.textCount = novel.textCount
+            }
+            else{
+                novel.seriesId = novel.id  // 获取系列小说id
+                // novel.title = novel.title
+                novel.url = novel.cover.urls["480mw"]
+                novel.description = novel.caption
+                novel.textCount = novel.textLength
+                // novel.id = novel.latestEpisodeId  // 最近一篇
+                // novel.lastChapter = this.getAjaxJson(this.urlNovelDetailed(novel.id)).body.title
+            }
 
             let userAllWorks = util.getAjaxJson(util.urlUserAllWorks(novel.userId)).body
             for (let series of userAllWorks.novelSeries) {
@@ -161,71 +182,9 @@ function getUserNovels(username) {
     return novels
 }
 
-
-function isLogin() {
-    let cookie = String(java.getCookie("https://www.pixiv.net/", null))
-    return typeof cookie === "string" && cookie !== ""
-}
-
-function getUserNovels(username) {
-    if (!isLogin()) {
-        return []
-    }
-
-    let html = java.ajax(util.urlSearchUser(username))
-    // java.log(html)
-    // 仅匹配有投稿作品的用户
-    let match = html.match(new RegExp(`"userIds":\\[(?:(?:\\d+,?)+)]`))
-    // ["\"userIds\":[34568581,4569033,3024386]"]
-    // java.log(JSON.stringify(match))
-    if (match === null || match.length === 0) {
-        html = java.ajax(util.urlSearchUserPartial(username))
-        match = html.match(new RegExp(`"userIds":\\[(?:(?:\\d+,?)+)]`))
-        if (match === null || match.length === 0) {
-            return []
-        }
-    }
-
-    match = JSON.stringify(match).replace("\\","").split(",")
-    // java.log(JSON.stringify(match))
-    let regNumber = new RegExp("\\d+")
-    let uidList = match.map(v => {
-        return v.match(regNumber)[0]
-    })
-
-    // 仅限3个作者
-    java.log(JSON.stringify(uidList))
-    if (uidList.length >= 3) {
-        uidList.length = 3
-    }
-
-    let novels = []
-    let page = Number(java.get("page"))
-
-    uidList.forEach(id => {
-        let r = util.getAjaxJson(util.urlUserAllWorks(id))
-        let novelsId = Object.keys(r.body.novels).reverse().slice((page - 1) * 20, page * 20)
-        let url = util.urlUserNovels(id, novelsId)
-        util.debugFunc(() => {
-            java.log(`发送获取作者小说的Ajax请求:${url}`)
-        })
-        let userNovels = util.getWebviewJson(url, html => {
-            return (html.match(new RegExp(">\\{.*?}<"))[0].replace(">", "").replace("<", ""))
-        }).body
-        // 获取对应的小说 该序列是按照id排序
-        // 反转以按照更新时间排序
-        novels = novels.concat(Object.values(userNovels).reverse())
-    })
-
-    util.debugFunc(() => {
-        java.log(`获取用户搜索小说结束`)
-    })
-    return novels
-}
-
 (() => {
     let novelsList = []
-    novelsList = novelsList.concat(JSON.parse(result).body.novel.data)
+    novelsList = novelsList.concat((JSON.parse(result).body.novel.data))
     novelsList = novelsList.concat(getUserNovels(String(java.get("key"))))
-    return util.formatNovels(handNovels(util.combineNovels(novelsList)))
+    return util.formatNovels(handNovels(novelsList))
 })();
