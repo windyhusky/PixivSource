a16fa6b58b3482b5bec90b34ef3796f47d7c5295
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2024-12-18 17:57:10 +0800

    Linpx 详情：单篇系列"分流"

--- linpx/detail.js
+++ linpx/detail.js
@@ -10,34 +10,47 @@ function objParse(obj) {
     })
 }
 
+function novelHandler(res){
+    let info = {}
+    info.novelId = res.id
+    // info.title = res.title
+    info.userName = res.userName
+    info.tags = res.tags
+    info.textCount = res.length
+    info.description = res.desc
+    info.coverUrl = util.urlCoverUrl(res.coverUrl)
+    // info.catalogUrl = util.urlNovelDetailed(res.id)
+    info.createDate = util.timeTextFormat(res.createDate)
+
+    if (res.series === undefined || res.series === null) {
+        info.title = info.latestChapter = info.title
+        info.tags.unshift('单本')
+        info.catalogUrl = util.urlNovelDetailed(info.novelId)
+    } else {
+        info.seriesId = res.series.id
+        info.title = res.series.title
+        info.tags.unshift('长篇')
+        info.catalogUrl = util.urlSeriesDetailed(info.seriesId)
+
+        let res2 = util.getAjaxJson(util.urlSeriesDetailed(info.seriesId))
+        info.tags = info.tags.concat(res2.tags)   //合并系列 tags
+        info.tags = Array.from(new Set(info.tags))
+        info.description = `${res2.caption}\n当前章节简介：\n${info.description}`
+    }
+    info.tags = info.tags.join(",")
+    if (util.MORE_INFO_IN_DESCRIPTION) {
+        info.description = `书名：${info.title}\n作者：${info.userName}\n标签：${info.tags}\n更新：${info.time}\n简介：${info.description}`
+    } else {
+        info.description = `${info.description}\n更新时间：${info.time}`
+    }
+    return info
+}
+
 (function (res) {
-    // 为了兼顾导入书架直接走详情页逻辑
-    // 这里不能直接用 book.xxx 来复用搜索页处理结果
-    res = util.getNovelRes(result)
+    // 为了兼顾导入书架直接走详情页逻辑，不能直接用 book.xxx 来复用搜索页处理结果
+    res = util.getNovelRes(result)  // 系列数据过少，暂不分流
     try {
-        let info = {}
-        info.userName = res.userName
-        info.tags = res.tags
-        info.textCount = res.length
-        info.description = res.desc
-        info.coverUrl = util.urlCoverUrl(res.coverUrl)
-
-        if (res.series === undefined || res.series === null) {
-            info.title = info.latest_chapter = res.title
-            info.tags.unshift('单本')
-            info.catalog = util.urlNovelDetailed(res.id)
-        } else {
-            info.title = res.series.title
-            info.tags.unshift('长篇')
-            info.catalog = util.urlSeriesDetailed(res.series.id)
-        }
-        info.tags = info.tags.join(",")
-        // info.description = `${info.description}\n更新时间：${info.time}`
-        if (util.MORE_INFO_IN_DESCRIPTION) {
-            info.description = `书名：${info.title}\n作者：${info.userName}\n标签：${info.tags}\n更新：${info.time}\n简介：${info.description}`
-        }
-        return info
-
+        return novelHandler(res)
     }catch (e) {
         java.log(e)
         java.log(`受 Linpx 的限制，无法获取当前小说的数据`)
