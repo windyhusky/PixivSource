f0898c1175fa849ab2e16d35839b13c55af758fc
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-01-26 16:03:18 +0800

    发现：精简发现地址2

--- linpx/discover_address.js
+++ linpx/discover_address.js
@@ -1,16 +1,14 @@
 @js:
 li = [
-    {
-        "title": "推荐作者",
-        "url": "https://api.furrynovel.ink/fav/user/cache"
-    },
-    {
-        "title": "最新小说",
-        "url": "https://api.furrynovel.ink/pixiv/novels/recent/cache?page={{page}}"
-    }
+    {"推荐作者": "https://api.furrynovel.ink/fav/user/cache"},
+    {"最新小说": "https://api.furrynovel.ink/pixiv/novels/recent/cache?page={{page}}"}
 ]
 
+// 格式化发现地址
 li.forEach(item => {
+    item.title = Object.keys(item)[0]
+    item.url = Object.values(item)[0]
+    delete item[Object.keys(item)[0]]
     item.style = {}
     item.style.layout_flexGrow = 1
     item.style.layout_flexBasisPercent = 0.15
