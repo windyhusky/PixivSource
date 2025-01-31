ce4d79409e02b392a64ebddc2ca5e7c3c3f5b2ed
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-01-31 19:33:43 +0800

    FN 目录：支持添加链接

--- furrynovel/catalog.js
+++ furrynovel/catalog.js
@@ -21,7 +21,25 @@ function novelHandler(novels) {
     return novels
 }
 
+function getNovelRes(result){
+    let res = {}
+    let isHtml = result.startsWith("<!DOCTYPE html>")
+    let pattern = "(https?://)?(www\\.)?furrynovel\\.com/(zh|en|ja)/novel/\\d+(/chapter/d+)?"
+    let fnWebpage = baseUrl.match(new RegExp(pattern))
+    if (isHtml && fnWebpage) {
+        let novelId = baseUrl.match(new RegExp("\\d+"))[0]
+        // res = util.getAjaxJson(util.urlNovelDetail(novelId))
+        res = util.getAjaxJson(util.urlNovelChapterInfo(novelId))
+    } else {
+        res = JSON.parse(result)
+    }
+    if (res.data.length === 0) {
+        java.log(`无法从 FurryNovel.com 获取当前小说`)
+        java.log(JSON.stringify(res))
+    }
+    return res.data
+}
+
 (function () {
-    res = JSON.parse(result).data
-    return novelHandler(res)
+    return novelHandler(getNovelRes(result))
 })()
