ac52c6b3ac8b60ac6fa4f3b36f0f41f2a1fd6616
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-02-01 12:16:23 +0800

    添加搜索地址

---
+++ linpx/searchUrl.js
@@ -0,0 +1,3 @@
+@js:
+java.put("page",page);java.put("key",key);
+`https://api.furrynovel.ink/pixiv/search/novel/${encodeURI(key)}/cache?page=${page}`;
---
+++ pixiv/searchUrl.js
@@ -0,0 +1,3 @@
+@js:
+java.put("page",page);java.put("key",key);
+`https://www.pixiv.net/ajax/search/novels/${encodeURI(key)}?word=${encodeURI(key)}&order=date_d&mode=all&p=${page}&s_mode=s_tag&gs=1&lang=zh`;
