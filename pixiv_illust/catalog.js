85931f9ca5e3bf1c07fcd5f32f87414b682cfeed
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-03-13 22:46:58 +0800

    Pixiv 插画：目录

---
+++ pixiv_illust/catalog.js
@@ -0,0 +1,34 @@
+@js:
+var util = objParse(String(java.get("util")))
+
+function objParse(obj) {
+    return JSON.parse(obj, (n, v) => {
+        if (typeof v == "string" && v.match("()")) {
+            return eval(`(${v})`)
+        }
+        return v;
+    })
+}
+
+function urlIllust(novelId){
+    if (util.SHOW_ORIGINAL_NOVEL_LINK) {
+        return urlIllustUrl(novelId)
+    } else {
+        return urlIllustDetailed(novelId)
+    }
+}
+
+function oneShotHandler(res) {
+    return [{
+        title: res.title.replace(RegExp(/^\s+|\s+$/g), ""),
+        // chapterUrl: urlIllust(res.id),
+        chapterUrl: urlIllustDetailed(res.id),
+        chapterInfo: `${timeTextFormat(res.createDate)}`
+    }]
+}
+
+
+(() => {
+    return oneShotHandler(JSON.parse(result).body)
+
+})()
