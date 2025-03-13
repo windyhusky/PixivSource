a5a23cb57071c205bd14e350da8810af55a2aba7
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-03-13 22:47:06 +0800

    Pixiv 插画：正文

---
+++ pixiv_illust/content.js
@@ -0,0 +1,29 @@
+@js:
+var util = objParse(String(java.get("util")))
+
+function objParse(obj) {
+    return JSON.parse(obj, (n, v) => {
+        if (typeof v == "string" && v.match("()")) {
+            return eval(`(${v})`)
+        }
+        return v;
+    })
+}
+
+function getContent(res) {
+    let content = [""]
+    li = "mini thumb small regular original".split(" ")
+    let illustLink = getAjaxJson(urlIllustDetailed(res.id)).body.urls.original
+    java.log(illustLink)
+
+    for (let order = 0; order < res.pageCount; order++) {
+        content.push(`<img src="${illustLink}">`)
+        illustLink = illustLink.replace(`_p${order}`, `_p${order + 1}`)
+    }
+    content = content.join("\n")
+    return content
+}
+
+(function () {
+    return getContent(JSON.parse(result).body)
+})()
