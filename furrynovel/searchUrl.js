20eb441a0e4569e1daad93ada65f049266d7b4a8
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-03-25 21:12:41 +0800

    Linpx & FN: 搜索时不转码

--- furrynovel/searchUrl.js
+++ furrynovel/searchUrl.js
@@ -1,3 +1,3 @@
 @js:
 java.put("page",page);java.put("key",key);
-`https://api.furrynovel.com/api/zh/novel?page=${page}&order_by=popular&keyword=${encodeURI(key)}`;
+`https://api.furrynovel.com/api/zh/novel?page=${page}&order_by=popular&keyword=${key}`;
