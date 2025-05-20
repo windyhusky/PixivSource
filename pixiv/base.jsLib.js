69d70f08d76eb46303787c5f0a67410e06190b5a
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-05-20 23:40:35 +0800

    Pixiv JSLib: 延长缓存时间至2h

--- pixiv/base.jsLib.js
+++ pixiv/base.jsLib.js
@@ -1,11 +1,10 @@
 var checkTimes = 0
-let time = 0
 
 function cacheGetAndSet(cache, key, supplyFunc) {
     let v = cache.get(key)
     if (v === undefined || v === null) {
         v = JSON.stringify(supplyFunc())
-        cache.put(key, v, 30*60) // 缓存 30 min
+        cache.put(key, v, 2*60*60) // 缓存 2h
     }
     return JSON.parse(v)
 }
