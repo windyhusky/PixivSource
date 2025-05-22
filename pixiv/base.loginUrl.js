3b4adba8d1cfa8a13b530048b9325c5f067ccbcc
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-05-23 00:39:33 +0800

    Pixiv base: 添加收藏使用 getPostResultBody 进行请求

--- pixiv/base.loginUrl.js
+++ pixiv/base.loginUrl.js
@@ -1,14 +1,13 @@
-
-function getPostReusult(url, body, headers) {
+function getPostResultBody(url, body, headers) {
+    headers = JSON.parse(cache.get("headers"))
     return JSON.parse(java.post(url, body, headers).body())
 }
 
 function novelBookmarkAdd(novelId, restrict=0) {
-    let url  = "https://www.pixiv.net/ajax/novels/bookmarks/add"
-    let body = {"novel_id": novelId, "restrict": restrict, "comment":"", "tags":[]}
-    let headers = JSON.parse(cache.get("headers"))
-
-    let resp = JSON.parse(java.post(url, JSON.stringify(body), headers).body())
+    let resp = getPostResultBody(
+        "https://www.pixiv.net/ajax/novels/bookmarks/add",
+        JSON.stringify({"novel_id": novelId, "restrict": restrict, "comment":"", "tags":[]})
+    )
     if (resp.error === true) sleepToast("收藏失败")
     else if (resp.body === null) sleepToast("已经收藏")
     else cache.put(`collect${novelId}`, resp.body); sleepToast("收藏成功")
