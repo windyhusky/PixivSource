42a13682c24f8832bd273b236f88e37e1d2b5b3f
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-06-05 14:13:08 +0800

    Pixiv 设置：关闭快速模式后，恢复默认状态

--- pixiv/base.loginCheckJS.js
+++ pixiv/base.loginCheckJS.js
@@ -50,6 +50,11 @@ function publicFunc() {
         u.SHOW_UPDATE_TIME = settings.SHOW_UPDATE_TIME = true       // 目录：显示章节更新时间
         u.SHOW_ORIGINAL_LINK = settings.SHOW_ORIGINAL_LINK = false  // 目录：显示章节源链接
         u.SHOW_CAPTIONS = settings.SHOW_CAPTIONS = true             // 正文：显示评论
+    } else {
+        u.CONVERT_CHINESE = settings.CONVERT_CHINESE = true         // 搜索：繁简通搜
+        u.SHOW_UPDATE_TIME = settings.SHOW_UPDATE_TIME = true       // 目录：显示章节更新时间
+        u.SHOW_ORIGINAL_LINK = settings.SHOW_ORIGINAL_LINK = true   // 目录：显示章节源链接
+        u.SHOW_CAPTIONS = settings.SHOW_CAPTIONS = true             // 正文：显示评论
     }
     cache.put("pixivSettings", JSON.stringify(settings))  // 设置写入缓存
 
