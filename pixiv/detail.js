c4d2b6d89eab9551934cea59e4b66e6822b67c00
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-06-05 19:43:49 +0800

    Pixiv 详情：优化书籍链接

--- pixiv/detail.js
+++ pixiv/detail.js
@@ -13,11 +13,11 @@ function objParse(obj) {
 function novelHandler(novel){
     novel = util.formatNovels(util.handNovels([novel], true))[0]
     if (novel.seriesId === undefined || novel.seriesId === null) {
-        novel.detailedUrl = urlNovelUrl(novel.id)
-        novel.catalogUrl = urlNovelDetailed(novel.id)
+        book.bookUrl = novel.detailedUrl = urlNovelUrl(novel.id)
+        book.tocUrl = novel.catalogUrl = urlNovelDetailed(novel.id)
     } else {
-        novel.detailedUrl = urlSeriesUrl(novel.seriesId)
-        novel.catalogUrl = urlSeriesDetailed(novel.seriesId)
+        book.bookUrl = novel.detailedUrl = urlSeriesUrl(novel.seriesId)
+        book.tocUrl = novel.catalogUrl = urlSeriesDetailed(novel.seriesId)
     }
     source.putLoginInfo(JSON.stringify(novel))  // 放入信息以便登陆界面使用
     return novel
