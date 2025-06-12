9473cbb8d16105f032ac297d830684bbf3b5c10f
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-06-12 19:54:46 +0800

    Pixiv JSLib：更新 isSourceRead

--- pixiv/base.jsLib.js
+++ pixiv/base.jsLib.js
@@ -14,8 +14,13 @@ function isBackupSource() {
 // 可用 source.bookSourceName source.getVariable() 等
 // java.getUserAgent() java.getWebViewUA() 目前返回内容相同
 function isSourceRead() {
-    const {java} = this
-    return java.getUserAgent() === java.getWebViewUA()
+    const {java, cache} = this
+    let isSourceRead = JSON.parse(cache.get("isSourceRead"))
+    if (isSourceRead === null) {
+        isSourceRead = (java.getUserAgent() === java.getWebViewUA())
+        cache.put("isSourceRead", JSON.stringify(isSourceRead))
+    }
+    return isSourceRead
 }
 
 function cacheGetAndSet(cache, key, supplyFunc) {
