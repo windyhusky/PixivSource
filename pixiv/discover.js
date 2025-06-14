ef2f87c8ffda72442c883507a8e43c0436d2f670
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-06-14 13:32:17 +0800

    Pixiv 发现：兼容源阅

--- pixiv/discover.js
+++ pixiv/discover.js
@@ -16,9 +16,9 @@ function handlerFactory() {
         return () => {updateSource(); return []}
     }
     // cache.delete("csfrToken")
-    if (!util.isLogin()) {
-        return handlerNoLogin()
-    }
+    // if (!util.isLogin()) {
+    //     return handlerNoLogin()
+    // }
     if (baseUrl.includes("/bookmark")) {
         return handlerBookMarks()
     }
@@ -125,9 +125,21 @@ function handlerWatchList() {
 
 // 排行榜，书签，首页，编辑部推荐，顺序相同
 function handlerRanking() {
-    if (isSourceRead()) return handlerRankingWebview()
-    // if (isSourceRead()) return handlerRankingAjax()
-    else return handlerRankingAjaxAll()
+    return () => {
+        let  novelIds = [], novelUrls = []
+        // let result = result + java.ajax(`${baseUrl}&p=2`)  // 正则获取网址中的 novelId
+        let matched = result.match(RegExp(/\/novel\/show\.php\?id=\d{5,}/gm))
+        for (let i in matched) {
+            let novelId = matched[i].match(RegExp(/\d{5,}/))[0]
+            if (novelIds.indexOf(novelId) === -1) {
+                novelIds.push(novelId)
+                novelUrls.push(urlNovelDetailed(novelId))
+            }
+        }
+        // java.log(JSON.stringify(novelIds))
+        let novels = getAjaxAllJson(novelUrls).map(resp => resp.body)
+        return util.formatNovels(util.handNovels(util.combineNovels(novels)))
+    }
 }
 
 // 排行榜，书签，首页，编辑部推荐，顺序相同
