f58f3be37b59c1b0bf4c488b906aaf881e740dfb
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-06-16 10:28:44 +0800

    Pixiv JSlib：修改 sleepToast 最短时长

--- pixiv/base.jsLib.js
+++ pixiv/base.jsLib.js
@@ -183,7 +183,7 @@ function sleepToast(text, second) {
     const {java} = this
     java.log(text)
     java.longToast(text)
-    if (second === undefined || second <= 3) second = 3
+    if (second === undefined) second = 0.01
     sleep(1000*second)
 }
 
