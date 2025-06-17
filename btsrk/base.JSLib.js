88ec5ac104c373c4b4f6e00148fd3113105be9f3
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-06-17 11:48:08 +0800

    Pixiv 订阅 JSSLib：缩短 sleepToast 的时间

--- btsrk/base.JSLib.js
+++ btsrk/base.JSLib.js
@@ -19,7 +19,7 @@ function sleepToast(text, second) {
     let {java} = this
     java.log(text)
     java.longToast(text)
-    if (second === undefined || second <= 3) {second = 3}
+    if (second === undefined) {second = 0.01}
     sleep(1000*second)
 }
 
