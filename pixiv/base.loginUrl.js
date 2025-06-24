bec0d03efd83c50ac63c6c12ca53b985bbcbbadf
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-06-24 18:45:36 +0800

    Pixiv 登录：添加设置

--- pixiv/base.loginUrl.js
+++ pixiv/base.loginUrl.js
@@ -415,14 +415,16 @@ function readMeSearch() {
 }
 
 let settingsName = {
+    "SEARCH_AUTHOR": "🔍 搜索作者",
     "CONVERT_CHINESE": "🀄️ 繁简通搜",
     "SHOW_UPDATE_TIME": "📅 更新时间",
     "SHOW_ORIGINAL_LINK": "🔗 原始链接",
     "SHOW_COMMENTS": "💬 显示评论",
-    "SEARCH_AUTHOR": "🔍 搜索作者",
     "MORE_INFORMATION": "📖 更多简介",
     "REPLACE_TITLE_MARKS": "📚 恢复《》",
     "SHOW_CAPTIONS": "🖼️ 显示描述",
+    "SHOW_LIKE_NOVELS" :"❤️ 显示收藏",
+    "SHOW_WATCHED_SERIES" :"📃 显示追更",
     "FAST": "⏩ 快速模式",
     "DEBUG": "🐞 调试模式"
 }
