a5097e7e99c180f50958aee00a61ffa5ea48efce
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-09-12 00:06:48 +0800

    Pixiv 登陆检测：作者名称前添加@，以便搜索作者

--- pixiv/base.loginCheckJS.js
+++ pixiv/base.loginCheckJS.js
@@ -415,6 +415,7 @@ function publicFunc() {
         novels = util.novelFilter(novels)
         novels.forEach(novel => {
             if (novel.title) novel.title = novel.title.replace(RegExp(/^\s+|\s+$/g), "")
+            if (!novel.userName.startsWith("@")) novel.userName = `@${novel.userName}`
             novel.coverUrl = urlCoverUrl(novel.coverUrl)
             novel.readingTime = `${novel.readingTime / 60} 分钟`
             novel.createDate = dateFormat(novel.createDate)
