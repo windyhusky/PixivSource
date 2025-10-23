eedefb72be306fbb7f7eb6057975ce769a423b7a
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-10-23 18:03:11 +0800

    FN 同步 JSLib 修改

--- furrynovel/base.jsLib.js
+++ furrynovel/base.jsLib.js
@@ -1,28 +1,37 @@
-var cacheSaveSeconds = 7*24*60*60  // 缓存时间7天
-
+var cacheSaveSeconds = 7*24*60*60  // 长期缓存时间 7天
+var cacheTempSeconds = 10*60*1000  // 短期缓存 10min
 
 function cacheGetAndSet(cache, key, supplyFunc) {
     let v = cache.get(key)
+    // 缓存信息错误时，保存 10min 后重新请求
+    if (v && JSON.parse(v).code === 404) {
+        if (new Date().getTime() >= JSON.parse(v).timestamp + cacheTempSeconds) {
+            cache.delete(key)
+            v = cache.get(key)
+        }
+    }
+    // 无缓存信息时，进行请求
     if (v === undefined || v === null) {
-        v = JSON.stringify(supplyFunc())
-        cache.put(key, v, cacheSaveSeconds)  // 延长缓存时间至7天
+        v = supplyFunc()
+        v.timestamp = new Date().getTime()
+        v = JSON.stringify(v)
+        cache.put(key, v, cacheSaveSeconds)
     }
     return JSON.parse(v)
 }
-function getAjaxJson(url) {
+
+function getAjaxJson(url, forceUpdate) {
     const {java, cache} = this
+    let v = cache.get(url)
+    if (forceUpdate && v && new Date().getTime() >= JSON.parse(v).timestamp + cacheTempSeconds) cache.delete(url)
     return cacheGetAndSet(cache, url, () => {
         return JSON.parse(java.ajax(url))
     })
 }
 function getAjaxAllJson(urls, forceUpdate) {
     const {java, cache} = this
-    if (forceUpdate === true) {
-        let result = java.ajaxAll(urls).map(resp => JSON.parse(resp.body()))
-        cache.put(urls, JSON.stringify(result), cacheSaveSeconds)
-        for (let i in urls) cache.put(urls[i], JSON.stringify(result[i]), cacheSaveSeconds)
-        return result
-    }
+    let v = cache.get(urls)
+    if (forceUpdate && v && new Date().getTime() >= JSON.parse(v).timestamp + cacheTempSeconds) cache.delete(urls)
     return cacheGetAndSet(cache, urls, () => {
         let result = java.ajaxAll(urls).map(resp => JSON.parse(resp.body()))
         cache.put(urls, JSON.stringify(result), cacheSaveSeconds)
