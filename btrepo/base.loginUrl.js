ebadfeec8a7fd511e80e54aed2c5ad17cb3c1004
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-10-28 23:02:09 +0800

    订阅：项目主页 添加 TG频道及群组

--- btrepo/base.loginUrl.js
+++ btrepo/base.loginUrl.js
@@ -8,31 +8,6 @@ function getWebViewUA() {
 
 function login() {}
 
-function readMe() {
-    sleepToast(`\n📌 书源使用简要教程\n
-1️⃣ 导入书源：点击按钮，导入书源
-导入书源后，需要【在书源内登录账号】
-登录后，可在阅读内部搜索小说
-搜索时，需要开启代理/梯子等\n
-2️⃣ 加入书架：系列目录/小说正文页面
-点击按钮，刷新页面后后可添加至书架
-需要【导入书源】并【在书源内登录账号】\n
-3️⃣ 搜索小说：阅读内部搜索关键词
-也可添加小说至书架
-　 详细内容：见 🔍 搜索教程\n
-4️⃣ 详细教程：见 🔰 使用指南`)
-}
-
-function readMeSearch() {
-    sleepToast(`\n🔍 阅读内部搜索说明\n
-    标签之间需要以【空格】间隔
-    ➖ 排除标签：#标签1 -标签2
-    👤 作者专搜：@作者的名称
-    #️ 标签专搜：#标签1 标签2 
-    ⏬ 字数筛选1：#标签1 标签2 字数3k5
-    ⏬ 字数筛选2：@作者的名称 字数3w5`.replace("    ",""), 5)
-}
-
 function startBrowser(url, title) {
     let msg = "", headers = `{"headers": {"User-Agent":"${getWebViewUA()}"}}`
     if (url.includes("https://github.com")) {
@@ -40,6 +15,11 @@ function startBrowser(url, title) {
         else if (url.includes("doc")) msg += "🔰 使用指南"
         else msg += "⭐️ 收藏项目"
         msg += "\n\n即将打开 Github\n请确认已开启代理/梯子/VPN等"
+    } else if (url.includes("https://t.me")) {
+        if (url.includes("FurryReading")) msg += "🐺 兽人阅读频道"
+        else if (url.includes("joinchat")) msg += "🐞 反馈群组"
+        else msg += "⭐️ 收藏项目"
+        msg += "\n\n即将打开 Telegram\n请确认已开启代理/梯子/VPN等"
     }
     sleepToast(msg)
     java.startBrowser(`${url}, ${headers}`, title)
@@ -48,9 +28,18 @@ function startBrowser(url, title) {
 function startGithub() {
     startBrowser("https://github.com/windyhusky/PixivSource", "书源介绍")
 }
+function startGithubLogin() {
+    startBrowser("https://github.com/login?return_to=https://github.com/windyhusky/PixivSource", "收藏项目")
+}
 function startGithubIssue() {
     startBrowser("https://github.com/windyhusky/PixivSource/issues", "反馈问题")
 }
 function startGithubReadme() {
     startBrowser("https://github.com/windyhusky/PixivSource/blob/main/doc/Pixiv.md", "使用指南")
+}
+function startTelegram() {
+    startBrowser("https://t.me/FurryReading", "兽人阅读频道")
+}
+function startTelegramGroup() {
+    startBrowser("https://t.me/joinchat/ccb6zNXZ2R0zYmM9", "反馈群组")
 }
