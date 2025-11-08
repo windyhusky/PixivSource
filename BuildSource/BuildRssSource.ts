a511f7c2d830a6242a3c227834daa7de6924d825
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-11-08 16:53:26 +0800

    制作工具：订阅源 初始化

--- BuildSource/BuildRssSource.ts
+++ BuildSource/BuildRssSource.ts
@@ -43,15 +43,44 @@ function saveJsonFile(folder:string, fileName:string, data:any):void {
     console.log(`✅  ${outputPath} 生成成功`)
 }
 
-function buildRssSource(sourceName:string): RssSource {
+function buildRssSource(sourceName:string){
     sourceName = "btsrk"
+    // 需要在 项目根目录下执行
     let sourcePath = `rssSource/${sourceName}`
-    // let templatePath = `BuildSource/${sourceName}.json`
-    let templatePath = `BuildSource/rssSource.json`
+    let templatePath = `rssSource/${sourceName}/furry.json`
+    let defaultDataPath = `BuildSource/rssSource.json`
+    // console.log(sourcePath)
+    // console.log(templatePath)
+    // console.log(defaultDataPath)
 
     // 读取基础模板
-    const rssSource: RssSource = JSON.parse(readTextFile(templatePath))[0]
-    console.log(JSON.stringify(rssSource, null, 4))
+    const RssSource: RssSource[] = JSON.parse(readTextFile(templatePath))
+    const defaultData: RssSource = JSON.parse(readTextFile(defaultDataPath))[0]
 
-    return rssSource
+    // 填充默认数据
+    RssSource.forEach(item => {
+        item.articleStyle = defaultData.articleStyle
+        item.enableJs = defaultData.enableJs
+        item.enabled = defaultData.enabled
+        item.enabledCookieJar = defaultData.enabledCookieJar
+        item.lastUpdateTime = defaultData.lastUpdateTime
+        item.loadWithBaseUrl = defaultData.loadWithBaseUrl
+        item.showWebLog = defaultData.showWebLog
+        item.singleUrl = defaultData.singleUrl
+        item.type = defaultData.type
+    })
+
+
+    // item.header = defaultData.header
+    // item.injectJs = defaultData.injectJs
+    // item.jsLib = defaultData.jsLib
+
+    console.log(JSON.stringify(RssSource, null, 4))
+    return RssSource
 }
+
+function main() {
+    buildRssSource("")
+}
+
+main()
