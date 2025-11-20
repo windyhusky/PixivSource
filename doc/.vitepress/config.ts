100bec81f00c167a46532606f88553c3b249ccaa
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-11-21 02:45:12 +0800

    项目：添加 VitePress 配置2

--- doc/.vitepress/config.ts
+++ doc/.vitepress/config.ts
@@ -10,35 +10,41 @@ export default defineConfig({
         logo: "/pic/Legado-Pixiv.png",
         nav: [
             { text: "主页", link: "/" },
-            { text: "Pixiv 书源", link: "Pixiv" },
-            { text: "Linpx 书源", link: "Linpx" },
-            { text: "赞助名单", link: "Sponsor" }
+            { text: "Pixiv 书源", link: "/Pixiv" },
+            { text: "Linpx 书源", link: "/Linpx" },
+            { text: "赞助名单", link: "/Sponsor" }
         ],
         sidebar: [
             {
                 text: "专用教程",
                 items: [
-                    { text: "Pixiv 书源", link: "Pixiv" },
-                    { text: "Linpx 书源", link: "Linpx" },
+                    { text: "Pixiv 书源", link: "/Pixiv" },
+                    { text: "Linpx 书源", link: "/Linpx" },
+                    { text: "兽人控小说站 书源", link: "/FurryNovel" },
                 ]
             },
             {
                 text: "通用教程",
                 items: [
-                    { text: "下载阅读", link: "Download" },
-                    { text: "导入书源", link: "ImportBookSource" },
-                    { text: "导入订阅", link: "ImportRssSource" },
+                    { text: "下载阅读", link: "/Download" },
+                    { text: "导入书源", link: "/ImportBookSource" },
+                    { text: "导入订阅", link: "/ImportRssSource" },
+                    { text: "远程书籍", link: "/RemoteBooks" },
+                    { text: "备份恢复", link: "/WebdavBackup" },
+                    { text: "故障排查", link: "/TroubleShoot" }
                 ]
             },
             {
                 text: "赞助名单",
                 items: [
-                   { text: "赞助名单", link: "Sponsor" },
-               ]
+                    { text: "赞助名单", link: "/Sponsor" },
+                ]
             }
         ],
         socialLinks: [
             { icon: "github", link: "https://github.com/DowneyRem/PixivSource" },
+            { icon: "codeberg", link: "https://codeberg.org/DowneyRem/PixivSource" },
+            { icon: "telegram", link: "https://t.me/PixivSource" },
         ]
     }
 })
--- doc/index.md
+++ doc/index.md
@@ -8,7 +8,17 @@ hero:
   name: "PixivSource"
   text: "开源阅读 Pixiv 书源"
   tagline: 适配 开源阅读 Legado 3.0 的 Pixiv 书源
+#  image:
+#    src: "pic/Legado-Pixiv.png"
+#    alt: Pixiv Source Logo
   actions:
+    - theme: brand
+      text: ⬇️ 下载阅读 新共存版
+      link: "https://miaogongzi.lanzout.com/b01rgkhhe"
+    - theme: brand
+      text: ⬇️ 下载阅读 Plus
+      link: "https://github.com/Luoyacheng/legado/releases/latest"
+      
     - theme: alt
       text: 🚀 一键导入 Pixiv 书源
       link: "legado://import/importonline?src=https://cdn.jsdelivr.net/gh/DowneyRem/PixivSource@main/pixiv.json"
@@ -39,6 +49,33 @@ features:
   - icon: ✈️
     title: 直连无需代理
     details: 🅿️ 登录账号，开启直连<br>✈️ 无需代理，直接访问
-  
+    
+  - icon: 🔍
+    title: 高级搜索
+    details: 🀄️繁简通搜👤作者专搜<br>#️⃣标签专搜⏬字数过滤
+#    link: Pixiv.html#_1-阅读内部搜索
+    
+  - icon: 🌟
+    title: 定制发现
+    details: ⭐️关注📃追更💯推荐<br>❤️收藏👑排行🔥原创
+#    link: Pixiv.md#_2-发现更多小说
+
+  - icon: 🔗
+    title: 添加网址
+    details: 📄单篇📚系列👤作者<br>✅多条网址相继处理
+#    link: Pixiv.md#_3-添加网址
+    
+  - icon: 🌐
+    title: 订阅源
+    details: ✅添加小说<br>⏫导入/更新书源
+#    link: Pixiv.md#_4-订阅源
+
+#  - icon: 🚫
+#    title: 屏蔽功能
+#    details: 🚫屏蔽作者🚫屏蔽标签<br>🚫隐藏收藏🚫隐藏追更
+#
+#  - icon: ⚙️
+#    title: 设置灵活
+#    details: ✈️直连模式⚡️快速模式<br>🐞调试模式🔧默认设置
 ---
 
