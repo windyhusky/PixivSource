91ed4c4719bacb945f304c71cb3836731eeb06a9
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-11-21 05:08:48 +0800

    项目：添加 搜索提示翻译

--- doc/.vitepress/config.ts
+++ doc/.vitepress/config.ts
@@ -72,23 +72,20 @@ export default defineConfig({
             text: "在 GitHub 上编辑本文"
         },
         search: {
-            provider: 'local',
+            provider: "local",
             options: {
-                locales: {
-                    zh: {
-                        translations: {
-                            button: {
-                                buttonText: '搜索文档',
-                                buttonAriaLabel: '搜索文档'
-                            },
-                            modal: {
-                                noResultsText: '无法找到相关结果',
-                                resetButtonTitle: '清除查询条件',
-                                footer: {
-                                    selectText: '选择',
-                                    navigateText: '切换'
-                                }
-                            }
+                translations: {
+                    button: {
+                        buttonText: "搜索文档",
+                        buttonAriaLabel: "搜索文档"
+                    },
+                    modal: {
+                        noResultsText: "无法找到相关结果",
+                        resetButtonTitle: "清除查询条件",
+                        footer: {
+                            selectText: "选择",
+                            navigateText: "切换",
+                            closeText: "Esc 键",
                         }
                     }
                 }
