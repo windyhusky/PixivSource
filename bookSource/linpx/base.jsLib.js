0ecd7364ea903aa92b3ee169849f4a5299d6c085
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-11-25 02:21:01 +0800

    Linpx JSLib: 优化 cacheGetAndSet 参数

--- bookSource/linpx/base.jsLib.js
+++ bookSource/linpx/base.jsLib.js
@@ -1,7 +1,8 @@
 var cacheSaveSeconds = 7*24*60*60  // 长期缓存时间 7天
 var cacheTempSeconds = 10*60*1000  // 短期缓存 10min
 
-function cacheGetAndSet(cache, key, supplyFunc) {
+function cacheGetAndSet(key, supplyFunc) {
+    const {java, cache} = this
     let v = cache.get(key)
     // 缓存信息错误时，保存 10min 后重新请求
     if (v && JSON.parse(v).error === true) {
@@ -36,7 +37,7 @@ function getAjaxJson(url, forceUpdate) {
     const {java, cache} = this
     let v = cache.get(url)
     if (forceUpdate && v && new Date().getTime() >= JSON.parse(v).timestamp + cacheTempSeconds) cache.delete(url)
-    return cacheGetAndSet(cache, url, () => {
+    return this.cacheGetAndSet(url, () => {
         return JSON.parse(java.ajax(url))
     })
 }
@@ -44,7 +45,7 @@ function getAjaxAllJson(urls, forceUpdate) {
     const {java, cache} = this
     let v = cache.get(urls)
     if (forceUpdate && v && new Date().getTime() >= JSON.parse(v).timestamp + cacheTempSeconds) cache.delete(urls)
-    return cacheGetAndSet(cache, urls, () => {
+    return this.cacheGetAndSet(urls, () => {
         let result = java.ajaxAll(urls).map(resp => JSON.parse(resp.body()))
         cache.put(urls, JSON.stringify(result), cacheSaveSeconds)
         for (let i in urls) cache.put(urls[i], JSON.stringify(result[i]), cacheSaveSeconds)
@@ -53,7 +54,7 @@ function getAjaxAllJson(urls, forceUpdate) {
 }
 function getWebviewJson(url) {
     const {java, cache} = this
-    return cacheGetAndSet(cache, url, () => {
+    return this.cacheGetAndSet(url, () => {
         let html = java.webView(null, url, null)
         return JSON.parse((html.match(new RegExp(">\\[{.*?}]<"))[0].replace(">", "").replace("<", "")))
     })
