eeb44461713df32b31ab009879f97d166530633f
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-11-29 08:53:38 +0800

    Pixiv：自定义分享内容

--- bookSource/pixiv/content.callBack.js
+++ bookSource/pixiv/content.callBack.js
@@ -9,3 +9,12 @@ if (event === "clickBookName") {
 if (event === "clickAuthor") {
     java.searchBook(book.author)
 }
+// 覆盖阅读默认分享
+if (event === "clickShareBook") {
+    let text = `我正在看：【${book.author.replace("@", "")}】创作的《${book.name}》`
+    if (String(book.name) !== String(book.durChapterTitle)) {
+        text += `的 【${book.durChapterTitle}】`
+    }
+    text += `\n\n小说链接：\n${book.bookUrl}\n\n分享自【开源阅读】Pixiv书源。使用添加网址，快速添加本文`
+    java.copyText(text)
+}
