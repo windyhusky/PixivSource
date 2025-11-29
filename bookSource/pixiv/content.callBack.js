5e6bf860893b66857b1bfec6722260d3986a44a3
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-11-29 09:10:47 +0800

    Pixiv 回调：添加无用代码

--- bookSource/pixiv/content.callBack.js
+++ bookSource/pixiv/content.callBack.js
@@ -1,5 +1,5 @@
-java.log(event)
-java.log(typeof event)
+// java.log(event)
+// java.log(typeof event)
 
 // 恢复阅读搜索作者
 if (event === "clickBookName") {
@@ -18,3 +18,27 @@ if (event === "clickShareBook") {
     text += `\n\n小说链接：\n${book.bookUrl}\n\n分享自【开源阅读】Pixiv书源。使用添加网址，快速添加本文`
     java.copyText(text)
 }
+
+
+// // 开始阅读，登录界面添加当前章节名称
+// if (event === "startRead") {
+// }
+// // 保存阅读，登录界面添加当前章节名称
+// if (event === "saveRead") {
+// }
+// // 结束阅读
+// if (event === "endRead") {
+// }
+
+// // 添加书架
+// if (event === "addBookShelf") {}
+// // 移除书架
+// if (event === "delBookShelf") {}
+//
+// 自定义按钮
+// if (event === "clickCustomButton") {
+// }
+// // 开始书架刷新
+// if (event === "startShelfRefresh") {}
+// // 结束书架刷新
+// if (event === "endShelfRefresh") {}
