8b8c5471ce36f7d399235fb83929a28247802a74
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-12-08 21:57:09 +0800

    Linpx 目录：小说名称 trim

--- bookSource/linpx/catalog.js
+++ bookSource/linpx/catalog.js
@@ -17,35 +17,31 @@ function urlNovel(novelId) {
     }
 }
 
-function oneShotHandler(res) {
-    res.textCount =　res.content.length
-    res.updateDate = timeTextFormat(res.createDate)
+function oneShotHandler(resp) {
+    resp.textCount =　resp.content.length
+    resp.updateDate = timeTextFormat(resp.createDate)
     return [{
-        title: res.title.replace(RegExp(/^\s+|\s+$/g), ""),
-        chapterUrl: urlNovel(res.id),
-        chapterInfo:`${res.updateDate}　　${res.textCount}字`
+        title: resp.title.trim(),
+        chapterUrl: urlNovel(resp.id),
+        chapterInfo:`${resp.updateDate}　　${resp.textCount}字`
     }]
 }
 
-function seriesHandler(res) {
-    res.novels.forEach(v => {
-        v.title = v.title.replace(RegExp(/^\s+|\s+$/g), "").replace(RegExp(/（|）|-/g), "")
-        v.chapterUrl = urlNovel(v.id)
-        // v.updateDate = String(v.coverUrl.match(RegExp("\\d{4}/\\d{2}/\\d{2}")))  //fake
-        v.detail = getAjaxJson(urlNovelDetailed(v.id))
-        try{
-            v.textCount = v.detail.content.length
-            v.updateDate = timeTextFormat(v.detail.createDate)
-            v.chapterInfo = `${v.updateDate}　　${v.textCount}字`
-        } catch (e) {}
-        util.debugFunc(() => {
-            java.log(`${v.title}`)
-        })
+function seriesHandler(resp) {
+    resp.novels.forEach(novel => {
+        novel.title = novel.title.trim()
+        novel.chapterUrl = urlNovel(novel.id)
+        // novel.updateDate = String(novel.coverUrl.match(RegExp("\\d{4}/\\d{2}/\\d{2}")))  //fake
+        novel.detail = getAjaxJson(urlNovelDetailed(novel.id))
+        novel.textCount = novel.detail.content.length
+        novel.updateDate = timeTextFormat(novel.detail.createDate)
+        novel.chapterInfo = `${novel.updateDate}　　${novel.textCount}字`
+        delete novel.detail
     })
-    return res.novels
+    return resp.novels
 }
 
-function seriesHandler(resp) {
+function seriesContentHandler(resp) {
     let novels = [], prevNovels = [], nextNovels = []
     while (resp.series.prev !== null && resp.series.prev !== undefined) {
         prevNovels.push(resp.series.prev)
@@ -59,6 +55,7 @@ function seriesHandler(resp) {
     novels = novels.concat(prevNovels.reverse())
     novels = novels.concat(nextNovels)
     novels.forEach(novel => {
+        novel.title = novel.title.trim()
         novel.chapterUrl = urlNovel(novel.id)
         novel.detail = getAjaxJson(urlNovelDetailed(novel.id))
         novel.textCount = novel.detail.content.length
@@ -71,20 +68,10 @@ function seriesHandler(resp) {
 }
 
 (() => {
-    if (isJsonString(result)) {
-        result = JSON.parse(result)
-        if (result.series) {
-            return seriesHandler(result)
-        } else {
-            return oneShotHandler(result)
-        }
+    let resp = util.getNovelRes(result)
+    if (resp.novels !== undefined) {
+        return seriesHandler(resp) || seriesContentHandler(resp)
+    } else {
+        return oneShotHandler(resp)
     }
-
-    // let res = util.getNovelRes(result)
-    // java.log(JSON.stringify(res))
-    // if (res.novels !== undefined) {
-    //     return seriesHandler(res)
-    // } else {
-    //     return oneShotHandler(res)
-    // }
 })()
