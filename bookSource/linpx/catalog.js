b5fe3e4a299a823a14bbed4fc83742bf0f7f02ea
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-12-08 21:55:54 +0800

    Linpx 目录：通过正文获取被删除的系列目录

--- bookSource/linpx/catalog.js
+++ bookSource/linpx/catalog.js
@@ -45,11 +45,46 @@ function seriesHandler(res) {
     return res.novels
 }
 
-(() => {
-    let res = util.getNovelResSeries(result)
-    if (res.novels !== undefined) {
-        return seriesHandler(res)
-    } else {
-        return oneShotHandler(res)
+function seriesHandler(resp) {
+    let novels = [], prevNovels = [], nextNovels = []
+    while (resp.series.prev !== null && resp.series.prev !== undefined) {
+        prevNovels.push(resp.series.prev)
+        resp = getAjaxJson(urlNovelDetailed(resp.series.prev.id))
     }
+    nextNovels.push({id: resp.id, order: resp.series.order, title: resp.title})
+    while (resp.series.next !== null && resp.series.prev !== undefined) {
+        nextNovels.push(resp.series.next)
+        resp = getAjaxJson(urlNovelDetailed(resp.series.next.id))
+    }
+    novels = novels.concat(prevNovels.reverse())
+    novels = novels.concat(nextNovels)
+    novels.forEach(novel => {
+        novel.chapterUrl = urlNovel(novel.id)
+        novel.detail = getAjaxJson(urlNovelDetailed(novel.id))
+        novel.textCount = novel.detail.content.length
+        novel.updateDate = timeTextFormat(novel.detail.createDate)
+        novel.chapterInfo = `${novel.updateDate}　　${novel.textCount}字`
+        delete novel.detail
+    })
+    // java.log(JSON.stringify(novels))
+    return novels
+}
+
+(() => {
+    if (isJsonString(result)) {
+        result = JSON.parse(result)
+        if (result.series) {
+            return seriesHandler(result)
+        } else {
+            return oneShotHandler(result)
+        }
+    }
+
+    // let res = util.getNovelRes(result)
+    // java.log(JSON.stringify(res))
+    // if (res.novels !== undefined) {
+    //     return seriesHandler(res)
+    // } else {
+    //     return oneShotHandler(res)
+    // }
 })()
