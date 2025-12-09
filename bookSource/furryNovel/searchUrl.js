1bcbd3352347076d6b2de5eff7fd5ee159f2f6d5
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-12-10 00:14:14 +0800

    FN 搜索：支持 搜索作者和搜索标签

--- bookSource/furryNovel/searchUrl.js
+++ bookSource/furryNovel/searchUrl.js
@@ -1,3 +1,14 @@
 java.put("key", key)
 java.put("page", page)
+if (key.startsWith("@") || key.startsWith("＠")) {
+    key = key.slice(1)
+    java.log(`👤 搜索作者：${key}`)
+} else if (key.startsWith("#") || key.startsWith("＃")) {
+    key = key.slice(1)
+    java.log(`#️⃣ 搜索标签：${key}`)
+} else {
+    java.log(`🔍 搜索内容：${key}`)
+}
+
+java.put("key", key)
 urlSearchNovel(key, page)
