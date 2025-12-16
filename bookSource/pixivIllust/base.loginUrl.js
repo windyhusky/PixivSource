91a496ce465ddb475256a3560608a5d142089197
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-12-16 17:07:43 +0800

    Pixiv 漫画：优化漫画设置

--- bookSource/pixivIllust/base.loginUrl.js
+++ bookSource/pixivIllust/base.loginUrl.js
@@ -133,18 +133,10 @@ function startGithubReadme() {
 }
 
 let settingsName = {
-    "SEARCH_AUTHOR": "🔍 搜索作者",
     "SHOW_ORIGINAL_LINK": "🔗 原始链接",
     "CONVERT_CHINESE": "🀄️ 繁简通搜",
-    "SHOW_UPDATE_TIME": "📅 更新时间",
-    "SHOW_COMMENTS": "💬 显示评论",
-    "MORE_INFORMATION": "📖 更多简介",
-    "REPLACE_TITLE_MARKS": "📚 恢复《》",
-    "SHOW_CAPTIONS": "🖼️ 显示描述",
-    "SHOW_LIKE_NOVELS" :"❤️ 显示收藏",
-    "SHOW_WATCHED_SERIES" :"📃 显示追更",
+    "QUALITY_REGULAR": "🖼️ 常规质量",
     "IPDirect": "✈️ 直连模式",
-    "FAST": "⏩ 快速模式",
     "DEBUG": "🐞 调试模式"
 }
 
@@ -174,7 +166,7 @@ function getSettingStatus(mode) {
 }
 
 function showSettings() {
-    sleepToast(`⚙️ 当前设置\n\n${getSettingStatus()}`)
+    sleepToast(`\n⚙️ 当前设置\n\n${getSettingStatus()}`)
 }
 
 function setDefaultSettingsLoginUrl() {
