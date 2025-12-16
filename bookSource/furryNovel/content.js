f592ad0bea645ca888a40db81d15c016245ffaff
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-12-16 18:50:05 +0800

    FN 正文：同步代码

--- bookSource/furryNovel/content.js
+++ bookSource/furryNovel/content.js
@@ -9,8 +9,7 @@ function objParse(obj) {
     })
 }
 
-function getContent(res) {
-    let content = res.content
+function replaceUploadedImage(res, content) {
     // 获取 [uploadedimage:] 的图片链接
     let hasEmbeddedImages = content.match(RegExp(/\[uploadedimage:(\d+)-?(\d+)]/gm))
     if (hasEmbeddedImages) {
@@ -19,7 +18,9 @@ function getContent(res) {
             content = content.replace(`[uploadedimage:${key}]`, `<img src="${urlLinpxCoverUrl(resp.images[key].origin)}">`)
         })
     }
-
+    return content
+}
+function replacePixivImage(content) {
     // 获取 [pixivimage:] 的图片链接 [pixivimage:1234] [pixivimage:1234-1]
     let matched = content.match(RegExp(/\[pixivimage:(\d+)-?(\d+)]/gm))
     if (matched) {
@@ -39,17 +40,23 @@ function getContent(res) {
             }
         })
     }
-
+    return content
+}
+function replaceNewPage(content) {
     // 替换 Pixiv 分页标记符号 [newpage]
-    matched = content.match(RegExp(/[ 　]*\[newpage][ 　]*/gm))
-    if (matched) {
-        for (let i in matched) {
-            content = content.replace(`${matched[i]}`, `${"<p>​<p/>".repeat(3)}`)
+    if (!util.environment.IS_LEGADO) {
+        let matched = content.match(RegExp(/[ 　]*\[newpage][ 　]*/gm))
+        if (matched) {
+            for (let i in matched) {
+                content = content.replace(`${matched[i]}`, `${"<p>​<p/>".repeat(3)}`)
+            }
         }
     }
-
+    return content
+}
+function replaceChapter(content) {
     // 替换 Pixiv 章节标记符号 [chapter:]
-    matched = content.match(RegExp(/\[chapter:(.*?)]/gm))
+    let matched = content.match(RegExp(/\[chapter:(.*?)]/gm))
     if (matched) {
         for (let i in matched) {
             let matched2 = matched[i].match(/\[chapter:(.*?)]/m)
@@ -57,36 +64,45 @@ function getContent(res) {
             content = content.replace(`${matched[i]}`, `${chapter}<p>​<p/>`)
         }
     }
-
+    return content
+}
+function replaceJumpPage(content) {
     // 替换 Pixiv 跳转页面标记符号 [[jump:]]
-    matched = content.match(RegExp(/\[jump:(\d+)]/gm))
+    let matched = content.match(RegExp(/\[jump:(\d+)]/gm))
     if (matched) {
         for (let i in matched) {
             let page = matched[i].match(/\d+/)
             content = content.replace(`${matched[i]}`, `\n\n跳转至第${page}节`)
         }
     }
-
+    return content
+}
+function replaceJumpUrl(content) {
     // 替换 Pixiv 链接标记符号 [[jumpuri: > ]]
-    matched = content.match(RegExp(/\[\[jumpuri:(.*?)>(.*?)]]/gm))
+    let matched = content.match(RegExp(/\[\[jumpuri:(.*?)>(.*?)]]/gm))
     if (matched) {
         for (let i in matched) {
             let matched2 = matched[i].match(/\[\[jumpuri:(.*?)>(.*?)]]/m)
             let matchedText = matched2[0]
             let urlName = matched2[1].trim()
             let urlLink = matched2[2].trim()
-            // 阅读不支持超链接
-            //content = content.replace(`${matchedText}`, `<a href=${urlLink}>${urlName}</a>`)
-            if (urlLink === urlName) {
-                content = content.replace(`${matchedText}`, `${urlName}`)
+
+            if (util.environment.IS_LEGADO) {
+                content = content.replace(`${matchedText}`, `<a href=${urlLink}> ${urlName}</a>`)
             } else {
-                content = content.replace(`${matchedText}`, `${urlName}: ${urlLink}`)
+                if (urlLink === urlName) {
+                    content = content.replace(`${matchedText}`, `${urlName}`)
+                } else {
+                    content = content.replace(`${matchedText}`, `${urlName}: ${urlLink}`)
+                }
             }
         }
     }
-
+    return content
+}
+function replaceRb(content) {
     // 替换 Pixiv 注音标记符号 [[rb: > ]]
-    matched = content.match(RegExp(/\[\[rb:(.*?)>(.*?)]]/gm))
+    let matched = content.match(RegExp(/\[\[rb:(.*?)>(.*?)]]/gm))
     if (matched) {
         for (let i in matched) {
             let matched2 = matched[i].match(/\[\[rb:(.*?)>(.*?)]]/m)
@@ -94,7 +110,7 @@ function getContent(res) {
             let kanji = matched2[1].trim()
             let kana = matched2[2].trim()
 
-            if (!util.REPLACE_TITLE_MARKS) {
+            if (!util.settings.REPLACE_TITLE_MARKS) {
                 // 默认替换成（括号）
                 content = content.replace(`${matchedText}`, `${kanji}（${kana}）`)
             } else {
@@ -110,6 +126,19 @@ function getContent(res) {
             }
         }
     }
+    return content
+}
+
+function getContent(res) {
+    let content = res.content
+    // 替换 Pixiv 标记符
+    content = replaceUploadedImage(res, content)
+    content = replacePixivImage(content)
+    content = replaceNewPage(content)
+    content = replaceChapter(content)
+    content = replaceJumpPage(content)
+    content = replaceJumpUrl(content)
+    content = replaceRb(content)
     return content
 }
 
