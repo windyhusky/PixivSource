ba5195592de17edb8f452bac24a636cf34ca83b0
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-12-16 18:11:01 +0800

    Pixiv 漫画：发现添加强制登录

--- bookSource/pixivIllust/discover.js
+++ bookSource/pixivIllust/discover.js
@@ -58,8 +58,9 @@ function handlerFactory() {
 
 function handlerNoLogin() {
     return () => {
-        sleepToast("此功能需要在书源登录后才能使用")
-        sleepToast('发现 - 长按"Pixiv" - 登录 - 登录账号')
+        sleepToast("⚠️ 当前未登录账号\n\n请登录 Pixiv 账号", 1.5)
+        util.removeCookie(); util.login()
+        sleepToast("登录成功后，请重新进入发现", 2)
         return []
     }
 }
