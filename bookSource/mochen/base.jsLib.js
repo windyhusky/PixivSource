51b1bc147544e6b8d2b0a338fad76eb6fb040287
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-12-24 16:02:33 +0800

    墨辰整理：startBrowser 打开外部链接

--- bookSource/mochen/base.jsLib.js
+++ bookSource/mochen/base.jsLib.js
@@ -1,5 +1,22 @@
+function getWebViewUA() {
+    const {java, cache} = this
+    let userAgent = String(java.getWebViewUA())
+    if (userAgent.includes("Windows NT 10.0; Win64; x64")) {
+        userAgent = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Mobile Safari/537.36"
+    }
+    // java.log(`userAgent=${userAgent}`)
+    return String(userAgent)
+}
+
 function startBrowser(url, title) {
     const {java} = this
-    let headers = `{"headers": {"User-Agent":"${java.getWebViewUA()}"}}`
-    java.startBrowser(`${url}, ${headers}`, title)
+    if (!title) title = url
+    let headers = {}
+    headers["User-Agent"] = this.getWebViewUA()
+
+    if (url.endsWith(".json")) {
+        java.openUrl(`legado://import/importonline?src=${url}`)
+    } else {
+        java.startBrowser(`${url}, ${JSON.stringify({headers: headers})}`, title)
+    }
 }
