c50dc3ef1117ea11dc20935d197b09d59fc2447e
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2026-01-07 21:02:42 +0800

    站点：显示更新时间

--- doc/.vitepress/theme/index.ts
+++ doc/.vitepress/theme/index.ts
@@ -6,6 +6,7 @@ import SponsorTable from "./components/SponsorTable.vue";
 import DragonChat from './components/DragonChat.vue'
 import NotFound from "./components/404.vue";
 import HomeUnderline from "./components/HomeUnderline.vue"
+import UpdateTime from "./components/UpdateTime.vue"
 import './style/index.css'
 
 export default {
@@ -13,10 +14,12 @@ export default {
     Layout: () =>
         h(DefaultTheme.Layout, null, {
             "not-found": () => h(NotFound),
+            "doc-before": () => h(UpdateTime),
         }),
     enhanceApp({ app }) {
         app.component('SponsorTable', SponsorTable)
         app.component('DragonChat', DragonChat)
         app.component('HomeUnderline', HomeUnderline)
+        // app.component('UpdateTime', UpdateTime)
     }
 };
