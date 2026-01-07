f57bfffc132388d0344993a751a93940488f4463
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2026-01-07 19:50:10 +0800

    站点：更新 容器颜色

--- doc/.vitepress/theme/index.ts
+++ doc/.vitepress/theme/index.ts
@@ -1,16 +1,17 @@
 // .vitepress/theme/index.ts
-import DefaultTheme from "vitepress/theme"; // 1. 引入默认主题
-import { h } from "vue";
+import DefaultTheme from "vitepress/theme";
 
+import { h } from "vue";
 import SponsorTable from "./components/SponsorTable.vue";
 import DragonChat from './components/DragonChat.vue'
 import NotFound from "./components/404.vue";
+import './style/index.css'
 
 export default {
-    extends: DefaultTheme, // 2. 继承默认主题
+    extends: DefaultTheme,
     Layout: () =>
         h(DefaultTheme.Layout, null, {
-            "not-found": () => h(NotFound), // 你的 404 组件
+            "not-found": () => h(NotFound),
         }),
     enhanceApp({ app }) {
         app.component('SponsorTable', SponsorTable)
