c8f2309ac204a8cfa57c18f5286cc215becb3f78
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2026-02-05 17:43:00 +0800

    Pixiv JSLib: getAjaxAllJson 整体加入缓存

--- bookSource/pixiv/base.jsLib.js
+++ bookSource/pixiv/base.jsLib.js
@@ -120,9 +120,10 @@ function getAjaxJson(url, forceUpdate) {
 }
 function getAjaxAllJson(urls, forceUpdate) {
     const {java, cache} = this
-    return this.cacheGetAndSet(urls, () => {
+    let batchKey = JSON.stringify(urls)
+    return this.cacheGetAndSet(batchKey, () => {
         let results = java.ajaxAll(urls).map(resp => JSON.parse(resp.body()))
-        this.putInCacheObject(JSON.stringify(urls), results, cacheSaveSeconds)
+        this.putInCacheObject(batchKey, results, cacheSaveSeconds)
         for (let i in urls) this.putInCacheObject(urls[i], results[i], cacheSaveSeconds)
         return results
     }, forceUpdate)
