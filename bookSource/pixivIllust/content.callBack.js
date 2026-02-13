33537c4846000f2b420b58edf4f636b9e2344f80
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2026-02-13 22:08:12 +0800

    Pixiv 漫画：更新回调

--- bookSource/pixivIllust/content.callBack.js
+++ bookSource/pixivIllust/content.callBack.js
@@ -1,51 +1,96 @@
-// java.log(event)
-// java.log(typeof event)
+// function getNovel() {
+//     let novel = {}
+//     novel.author = novel.userName = book.author.replace("@", "")
+//     if (book.bookUrl.includes("series")) {
+//         novel.seriesId = book.bookUrl.match(/\d+/)[0]
+//         novel.seriesTitle = book.name
+//
+//         let novelIds = getFromCacheObject(`novelIds${novel.seriesId}`)
+//         novel.id = novelIds[book.durChapterIndex]
+//         novel.title = book.durChapterTitle
+//     } else {
+//         novel.seriesId = 0
+//         novel.seriesTitle = ""
+//         novel.id = book.bookUrl.match(/\d+/)[0]
+//         novel.title = book.name
+//     }
+//     return novel
+// }
 
-// 恢复阅读搜索书名
-if (event === "clickBookName") {
-    java.searchBook(book.name)
-}
-// 恢复阅读搜索作者
-if (event === "clickAuthor") {
-    java.searchBook(book.author)
-}
-// 覆盖阅读默认分享
-if (event === "clickShareBook") {
+function shareBook() {
     let text = `我正在看：【${book.author.replace("@", "")}】创作的《${book.name}》`
     if (!!book.durChapterTitle && String(book.name) !== String(book.durChapterTitle)) {
         text += `的 【${book.durChapterTitle}】`
     }
-    text += `\n\n小说链接：\n${book.bookUrl}\n\n分享自【开源阅读】Pixiv书源。使用添加网址，快速添加本文`
+    text += `\n\n链接：\n${book.bookUrl}\n\n分享自【开源阅读】Pixiv书源。使用添加网址，快速添加本文`
     java.copyText(text)
+    return true
+}
+
+function copyBookUrl() {
+    java.copyText(book.bookUrl)
+    return true
+}
+function copyTocUrl() {
+    java.copyText(book.tocUrl)
+    return true
 }
 
 // 保存阅读，更新登录界面的章节名称
-if (event === "saveRead") {
-    // sleepToast(book.durChapterTitle)
+function saveRead() {
     source.putLoginInfo(JSON.stringify({"章节名称": book.durChapterTitle}))
 }
 
-// 开始书架刷新
-if (event === "startShelfRefresh") {
+function startShelfRefresh() {
     source.putConcurrent("2/2000")
 }
-// 结束书架刷新
-if (event === "endShelfRefresh") {
+
+function endShelfRefresh() {
     source.putConcurrent("4/2000")
 }
 
-// // 开始阅读，登录界面添加当前章节名称
-// if (event === "startRead") {
-// }
-// // 结束阅读
-// if (event === "endRead") {
-// }
+function callBackFactory(event) {
+    switch (event) {
+        // case "clickBookName":
+        //     return clickBookName()
+        // case "longClickBookName":
+        //     return longClickBookName()
+        // case "clickAuthor":
+        //     return clickAuthor()
+        // case "longClickAuthor":
+        //     return longClickAuthor()
+        // case "clickCustomButton":
+        //     return customButton()
+        // case "longClickCustomButton":
+        //     return longcustomButton()
 
-// // 添加书架
-// if (event === "addBookShelf") {}
-// // 移除书架
-// if (event === "delBookShelf") {}
-//
-// 自定义按钮
-// if (event === "clickCustomButton") {
-// }
+        case "clickShareBook":
+            return shareBook()
+        // case "clickClearCache":
+        //     return clearCache()
+        case "clickCopyBookUrl":
+            return copyBookUrl()
+        case "clickCopyTocUrl":
+            return copyTocUrl()
+
+        // 下面的事件无法被回调结果消费
+        // case "addBookShelf":
+        //     return addBookShelf()
+        // case "delBookShelf":
+        //     return delBookShelf()
+        case "saveRead":
+            return saveRead()
+        // case "startRead":
+        //     return startRead()
+        // case "endRead":
+        //     return endRead()
+        case "startShelfRefresh":
+            return startShelfRefresh()
+        case "endShelfRefresh":
+            return endShelfRefresh()
+    }
+}
+
+(() => {
+    return callBackFactory(event)
+})()
