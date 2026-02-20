600963b653133081ed37557a62ab85d2d41f4f8f
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2026-02-20 21:21:11 +0800

    Pixiv 登录检测：修复屏蔽作者

--- bookSource/pixiv/base.loginCheckJS.js
+++ bookSource/pixiv/base.loginCheckJS.js
@@ -187,12 +187,10 @@ function publicFunc() {
     // 屏蔽作者
     u.authorFilter = function(novels) {
         let authors = getFromCacheObject("blockAuthorList")
-        if (Array.isArray(authors) && authors.length >= 0) {
+        if (Array.isArray(authors)) {
             java.log(`🚫 屏蔽作者ID：${JSON.stringify(authors)}`)
             authors.forEach(author => {
-                novels = novels.filter(novel => {
-                    novel.userId !== String(author)
-                })
+                novels = novels.filter(novel => novel.userId !== String(author))
             })
         }
         return novels
