51fce0b2bdd006735bdb638fa644ea22f5555dbe
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2026-05-08 16:43:03 +0800

    FN：优化 objParse

--- bookSource/furryNovel/content.js
+++ bookSource/furryNovel/content.js
@@ -2,7 +2,7 @@ var util = objParse(String(java.get("util")))
 
 function objParse(obj) {
     return JSON.parse(obj, (n, v) => {
-        if (typeof v == "string" && v.match("()")) {
+        if (isFunctionString(v)) {
             return eval(`(${v})`)
         }
         return v;
