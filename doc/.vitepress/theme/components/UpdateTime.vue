ef6b33176955eca63f77ec552586c41fd300c000
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2026-05-09 20:16:17 +0800

    站点：使用 lastUpdated 控制是否展示更新时间

--- doc/.vitepress/theme/components/UpdateTime.vue
+++ doc/.vitepress/theme/components/UpdateTime.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
 import { useData } from 'vitepress'
 import { computed } from 'vue'
-const { page } = useData()
 
-const date = computed(
-    () => new Date(page.value.lastUpdated!)
-)
+const { page, frontmatter } = useData()
+const showLastUpdated = computed(() => {
+  if (frontmatter.value.lastUpdated === false) return false
+  return !!page.value.lastUpdated
+})
+
+const date = computed(() => {
+  return page.value.lastUpdated ? new Date(page.value.lastUpdated) : null
+})
 </script>
 
 <template>
-    <div class="LastUpdated">
-        <p>更新时间: {{ date.toLocaleDateString() }}</p>
-    </div>
+  <div v-if="showLastUpdated" class="LastUpdated">
+    <p>更新时间: {{ date?.toLocaleDateString() }}</p><br>
+  </div>
 </template>
 
 <style>
