14be52ef8fb982aef3aae87791aa159b1493fe56
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2026-05-14 01:14:38 +0800

    书源：删除无用代码

--- bookSource/furryNovel/base.loginUrl.js
+++ bookSource/furryNovel/base.loginUrl.js
@@ -64,7 +64,6 @@ let settingsOptionsNames = {
         "PixivShojo": "📄 PixivShojo",
         "Pixiv": "🅿️ Pixiv 直连",
         "Linpx": "🦊 Linpx 网站",
-        // "FurryNovel": "🐯 FurryNovel",
         "PixivCat": "🐱 PixivCat",
         "CloudFlare": "☁️ CloudFlare",
     },
