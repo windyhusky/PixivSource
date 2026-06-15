4cc85ea33a4d6f8ba34aef388c95176054e1d709
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2026-06-15 13:24:44 +0800

    Pixiv 备用：更新变量名称 pixivUid

--- bookSource/pixivBackup/base.loginCheckJS.js
+++ bookSource/pixivBackup/base.loginCheckJS.js
@@ -120,7 +120,7 @@ function publicFunc() {
         cookie.removeCookie('https://accounts.google.com')
         cookie.removeCookie('https://api.weibo.com')
         cache.delete("pixivCookie")
-        cache.delete("pixiv:uid")
+        cache.delete("pixivUid")
         cache.delete("pixivCsrfToken")  // 与登录设备有关
         cache.delete("headers")
     }
@@ -585,7 +585,7 @@ function checkMessageThread(checkTimes) {
 
 // 获取请求的user id方便其他ajax请求构造
 function getPixivUid() {
-    // cache.delete("pixiv:uid")
+    // cache.delete("pixivUid")
     let pixivUid = getFromCache("pixivUid")
     if (!pixivUid) pixivUid = getFromCache("pixivUid")
 
