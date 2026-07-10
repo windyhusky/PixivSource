75e14a6fc93c52b657d8a19a362b5ebf545bcd5f
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2026-07-10 11:47:26 +0800

    Linpx：优化 isHtmlString

--- src/bookSource/linpx/base.jsLib.js
+++ src/bookSource/linpx/base.jsLib.js
@@ -63,7 +63,7 @@ function getFromCacheObject(objectName) {
 }
 
 function isHtmlString(str) {
-    return str.startsWith("<!DOCTYPE html>")
+    return str.startsWith("<!DOCTYPE html>") || str.startsWith("<!doctype html>")
 }
 function isJsonString(str) {
     try {
