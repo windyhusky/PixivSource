369c50829369213c0d32b3f18924fcdb3d69ad6a
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2026-09-07 15:35:16 +0800

    mochen：精简代码

--- src/bookSource/mochen/base.jsLib.js
+++ src/bookSource/mochen/base.jsLib.js
@@ -1,18 +1,8 @@
-function getWebViewUA() {
-    const {java, cache} = this
-    let userAgent = String(java.getWebViewUA())
-    if (userAgent.includes("Windows NT 10.0; Win64; x64")) {
-        userAgent = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Mobile Safari/537.36"
-    }
-    // java.log(`userAgent=${userAgent}`)
-    return String(userAgent)
-}
-
 function startBrowser(url, title) {
     const {java} = this
     if (!title) title = url
     let headers = {}
-    headers["User-Agent"] = this.getWebViewUA()
+    headers["User-Agent"] = String(java.getWebViewUA())
 
     if (url.endsWith(".json")) {
         java.openUrl(`legado://import/importonline?src=${url}`)
